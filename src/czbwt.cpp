06b7aa8fda4e6eba58e7be9e485720d2032d9def
Taylan Dogan <ttaylan.dogann@gmail.com>
2018-07-15 21:59:37 +0300

    There seems to be a bug in encode()

--- src/czbwt.cpp
+++ src/czbwt.cpp
@@ -15,28 +15,18 @@ tuple<int, string> encode(string s) {
   int j;
   string _3mer;
   for (int i=0; i < n; i++) {
-    if (i < 2) {
-      _3mer = s.substr(n+i-2) + s.substr(0, i+1);
-      // cout << "i: " << i << ", n+i-2: " << n+i-2 << endl;
-      // cout << "First part: '" << s.substr(n+i-2) << "'" << endl;
-      // cout << "Second part: '" << s.substr(0, i+1) << "'" << endl;
+    if (i + 2 >= n) {
+      _3mer = s.substr(i, n-i) + s.substr(0, 3-(n-i));
     }
     else {
-      _3mer = s.substr(i-2, 3);
+      _3mer = s.substr(i, 3);
     }
-    // Reverse string
-    // std::swap(_3mer[0], _3mer[2]);
-    cout << "_3mer: " << _3mer << endl;
-    // j = _3mer[0] + _3mer[1] * 256 + _3mer[2] * 256 * 256;
-    j = _3mer[0] * 256 * 256 + _3mer[1] * 256 + _3mer[2];
+
+    // cout << "_3mer: " << _3mer << endl;
+    j = _3mer[0] + _3mer[1] * 256 + _3mer[2] * 256 * 256;
+    // j = _3mer[0] * 256 * 256 + _3mer[1] * 256 + _3mer[2];
     cout << "For '" << _3mer << "' j is: " << j << endl;
     link[i] = bucket[j];
-    // stringstream sa;
-    // for (int z=0; z < n; z++) {
-    //   sa << link[z];
-    //   sa << ", ";
-    // }
-    // cout << sa.str() << endl;
     bucket[j] = i;
   }
 
@@ -44,24 +34,22 @@ tuple<int, string> encode(string s) {
   int count = 0;
   int start = 0;
   // Phase 2: Output data
-  int tmp;
-  int index;
-  int orig_index;
+  int i;
   stringstream output;
   for (int j = 0; j < bucket_size; j++) {
-    tmp = bucket[j];
-    while (tmp != -1) {
+    i = bucket[j];
+    while (i != -1) {
       // start stores the start position
-      if (tmp == (n-1)) {
-        cout << "TMP: " << tmp << " COUNT: " << count << endl;
+      if (i == (n-1)) {
+        cout << "i: " << i << " COUNT: " << count << endl;
         start = count;
       }
-      cout << "j: " << j << " TMP: " << tmp << endl;
+      cout << "j: " << j << " i: " << i << endl;
+
       // output a character of s
-      orig_index = (tmp + 1) % n;
-      index = (n + ((tmp - 3) % n)) % n;
+      int index = (i + 1) % n;
       output << s[index];
-      tmp = link[tmp];
+      i = link[i];
       count += 1;
     }
   }
